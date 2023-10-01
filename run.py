8e653496519dbe5d37ddbf69f565d1ff356a29a6
Vlad <vlad.a.borshch@gmail.com>
2023-10-01 16:04:29 +0300

    Lesson 7 Added admin-check, config_reader.py,

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
+import logging
 import asyncio
 from aiogram import Bot, Dispatcher
 from app.handlers import router
+from config_reader import config
+
+logging.basicConfig(level=logging.INFO)
 
 
 # Polling, т.е. бесконечный цикл проверки апдейтов на серверах Telegram
 async def main():
-    bot = Bot(token='6421951776:AAH5n4G5DsXFkRainmE7vFhENEeg6WEi6tI')
+    bot = Bot(token=config.bot_token.get_secret_value())
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
